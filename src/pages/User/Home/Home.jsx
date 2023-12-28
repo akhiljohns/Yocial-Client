0acaec4629e3d06de04f16cecbe25ac59b628663
Akhil Johns <akhiljohns250@gmail.com>
2023-12-29 00:40:55 +0530

    logout & user-details

--- src/pages/User/Home/Home.jsx
+++ src/pages/User/Home/Home.jsx
@@ -1,17 +1,10 @@
 import React from 'react';
-import {clearUser} from '../../../services/User/apiCalls.js'
-const Navbar = () => {
-  return (
-    <nav className="bg-gray-800 p-4 text-blac">
-      <div className="container mx-auto">
-        {/* Navbar content goes here */}
-        Navbar
-      </div>
-    </nav>
-  );
-};
+import { clearUser } from '../../../services/User/apiCalls.js';
+import { useSelector } from 'react-redux';
 
-const Header = ({ onLogout,name }) => {
+
+
+const Header = ({ onLogout, name }) => {
   return (
     <header className="bg-orange-500 p-4 text-white">
       <div className="container mx-auto flex justify-between items-center">
@@ -25,14 +18,22 @@ const Header = ({ onLogout,name }) => {
 };
 
 const Home = () => {
+  const userData = useSelector((state) => state?.user?.userData);
 
   return (
     <div>
-      <Header onLogout={() =>clearUser()} />
-      <div className="container mx-auto p-4">
-        {/* Page content goes here */}
-        <div>Home</div>
-      </div>
+      {userData && (
+        <>
+          <Header onLogout={() => clearUser()} name={userData.name} />
+          <div className="container mx-auto p-4">
+            {/* Page content goes here */}
+            <div id='homediv' className='flex flex-col items-center justify-center h-screen'>
+
+            <div className='text-white font-extrabold'>Home</div>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
