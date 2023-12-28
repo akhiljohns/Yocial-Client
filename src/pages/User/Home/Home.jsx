13680f25be31d02520278fc22e09c87022e0515a
Akhil Johns <akhiljohns250@gmail.com>
2023-12-29 00:30:12 +0530

    logout

--- src/pages/User/Home/Home.jsx
+++ src/pages/User/Home/Home.jsx
@@ -1,14 +1,8 @@
 import React from 'react';
-<<<<<<< HEAD
 import {clearUser} from '../../../services/User/apiCalls.js'
-=======
-import { useSelector } from 'react-redux';
-import { clearUser } from '../../../services/User/apiCalls';
-
->>>>>>> user-features
 const Navbar = () => {
   return (
-    <nav className="bg-gray-800 p-4 text-white">
+    <nav className="bg-gray-800 p-4 text-blac">
       <div className="container mx-auto">
         {/* Navbar content goes here */}
         Navbar
@@ -17,31 +11,24 @@ const Navbar = () => {
   );
 };
 
-const Header = ({ onLogout }) => {
+const Header = ({ onLogout,name }) => {
   return (
-    <header className="bg-blue-500 p-4 text-white">
+    <header className="bg-orange-500 p-4 text-white">
       <div className="container mx-auto flex justify-between items-center">
-        <div>Welcome, User!</div>
-        <button onClick={onLogout} className="bg-red-500 px-4 py-2 rounded">Logout</button>
+        <div>Welcome, {name}</div>
+        <button onClick={onLogout} className="bg-black px-4 py-2 rounded">
+          Logout
+        </button>
       </div>
     </header>
   );
 };
 
 const Home = () => {
-<<<<<<< HEAD
 
   return (
     <div>
       <Header onLogout={() =>clearUser()} />
-=======
-const userData = useSelector((state)=> state?.user?.userData)
-
-  return (
-    <div>
-      <Navbar />
-      <Header onLogout={() => clearUser()}  />
->>>>>>> user-features
       <div className="container mx-auto p-4">
         {/* Page content goes here */}
         <div>Home</div>
