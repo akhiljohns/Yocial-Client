7b5ac1f84e3a69ddf7a328fe16a90837e19a4563
Akhil Johns <akhiljohns250@gmail.com>
2023-12-29 15:56:16 +0530

    New Error Page

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,33 +1,23 @@
-import React from 'react'
-
-
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-
-
 function Error() {
-
-  const navigate = useNavigate()
-
+  const navigate = useNavigate();
 
   return (
-    <>
-      <div className="errorpage h-screen w-screen flex items-center justify-center">
-        <div className="fourNotFour -mt-32 flex items-end">
-          <div className="border-y-1 flex items-center">
-            <button className='goHomeButton'
-              onClick={() => {
-                navigate("/");
-              }}
-            >
-              <p>Error page go Home</p>
-            </button>
-          </div>
-        </div>
+    <div className="h-screen flex items-center justify-center bg-gray-900 text-white select-none">
+      <div className="text-center animate-fadeInUp">
+        <h1 className="text-6xl font-bold mb-4 animate-bounce">404</h1>
+        <p className="text-lg mb-8 animate-slideIn">Oops! Page not found.</p>
+        <button
+          onClick={() => navigate("/")}
+          className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700 transition-colors duration-300 ease-in-out"
+        >
+          Go Home
+        </button>
       </div>
-    </>
+    </div>
   );
 }
 
-
-export default Error
+export default Error;
