8b6fa8573d94eeb1c0999783b888f710cc6d64bc
Akhil Johns <akhiljohns250@gmail.com>
2024-01-01 01:02:36 +0530

    follow/unfollow ui

--- src/pages/User/Home/Home.jsx
+++ src/pages/User/Home/Home.jsx
@@ -1,42 +1,123 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import Header from '../../../components/user/Header/Header';
+import React, { useEffect, useState } from "react";
+import { fetchAllUsers } from "../../../services/Admin/apiMethods";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
+import { followUser, getConnections, unfollowUser } from "../../../services/User/apiMethods";
+import Header from "../../../components/user/Header/Header";
 
-// export const Header = ({ onLogout, userData }) => {
-//   const navigate = useNavigate()
-//   return (
-//     <header className="bg-orange-500 p-4 text-white">
-//       <div className="container mx-auto flex justify-between items-center">
-//         <div onClick={()=> navigate(`/profile/${userData.username}`)} >Welcome, {userData.name}</div>
-//         <button onClick={onLogout} className="bg-black px-4 py-2 rounded">
-//           Logout
-//         </button>
-//       </div>
-//     </header>
-//   );
-// };
+const UserHome = () => {
+  const navigate = useNavigate();
+  const currentUser = useSelector((state)=> state?.user?.userData);
+  const [following, setFollowing] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [error, setError] = useState("");
+  const { validUser, userData } = useSelector((state) => state?.user);
 
-const Home = () => {
-  const userData = useSelector((state) => state?.user?.userData);
+  useEffect(() => {
+    fetchAllUsers()
+      .then((response) => {
+        setUsers(response.users);
+      })
+      .catch((err) => {
+        setError(err.message);
+      });
+  }, []);
 
-  return (
-    <div>
-      {userData && (
-        <>
-          <Header />
-          <div className="container mx-auto p-4">
-            {/* Page content goes here */}
-            <div id='homediv' className='flex flex-col items-center justify-center h-screen'>
+  useEffect(()=> {
+    getConnections(currentUser?._id).then((connection)=>{
+        setFollowing(connection.following);
 
-            <div className='text-white font-extrabold'>Home</div>
-            </div>
+    }).catch((error)=>{
+        setError(error?.message);
+    })
+  }, [currentUser,following]);
+
+  const follow = (user) => {
+    followUser(currentUser?._id, user?._id)
+      .then((response)=> {
+        setFollowing(response.userConnection.following);
+        console.log(following);
+      })
+      .catch((error) => {
+        setError(error?.message);
+      });
+  }
+
+  const unfollow = (user) => {
+    unfollowUser(currentUser?._id, user?._id)
+      .then((response) => {
+        setFollowing(response.userConnection.following);
+      })
+      .catch((error) => {
+        setError(error?.message);
+      });
+  };
+
+  const UserRow = ({ user }) => (
+    <tr key={user._id}>
+      <td>
+        <div className="flex items-center">
+          <img src={user.profilePic} alt="" className="w-8 h-8 rounded-full mr-2" />
+          <div>
+            <a href="#" className="user-link">
+              {user.name}
+            </a>
+            <span className="user-subhead block">{user.role}</span>
           </div>
-        </>
-      )}
+        </div>
+      </td>
+      <td>{user.username}</td>
+      <td>
+        <div className="flex justify-around">
+          {
+            following?.includes(user?._id) ? (
+              <button onClick={() => follow(user)}>Follow</button>
+            ) : (
+              <button onClick={() => unfollow(user)}>Unfollow</button>
+            )
+          }
+        </div>
+      </td>
+    </tr>
+  );
+
+  const filteredUsers = users.filter(user => user._id !== currentUser?._id);
+  return (
+  <>
+  
+    <Header/>
+    <div className="flex items-center justify-center h-screen">
+      <div className="w-full max-w-4xl">
+        <div className="main-box bg-white p-6 rounded shadow-md">
+          <div className="table-responsive">
+            <table className="table user-list w-full">
+              <thead>
+                <tr>
+                  <th className="py-2">
+                    <span>Name</span>
+                  </th>
+                  <th className="py-2">
+                    <span>Username</span>
+                  </th>
+                  <th className="py-2">Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {filteredUsers.map((user) => (
+                  <UserRow key={user._id} user={user} />
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+      <ToastContainer />
     </div>
+    </>
   );
 };
 
-export default Home;
+export default UserHome;
