0b72f288b767e275eb52809644c38300047ea09f
akhiljohns <akhiljohns250@gmail.com>
2024-02-08 20:31:27 +0530

    saved Posts preview & comments preview added

--- src/pages/User/SavedPosts/SavedPosts.jsx
+++ src/pages/User/SavedPosts/SavedPosts.jsx
@@ -2,9 +2,14 @@ import React, { useEffect, useState } from "react";
 import Header from "../../../components/user/Header/Header";
 import { useSelector } from "react-redux";
 import { fetchSavedPosts } from "../../../services/User/apiMethods";
+import PostModal from "../../../components/user/Comments/PostModal";
+import { successToast } from "../../../hooks/toast";
 function SavedPosts() {
 
 const [savedPosts, setSavedPosts] = useState([]);
+const [postOpen, setPostOpen] = useState(false);
+const [selectedPost, setSelectedPost] = useState(null);
+const [commentCount, setCommentCount] = useState(0);
 const {userData} = useSelector((state) => state?.user);
 
 useEffect(()=>{
@@ -13,6 +18,11 @@ useEffect(()=>{
   })
 },[userData._id])
 
+
+const handlePostOpen = (post) => {
+setSelectedPost(post);
+setPostOpen(true);
+}
   return (
     <>
       <div className="fixed z-40">
@@ -23,6 +33,7 @@ useEffect(()=>{
           <div className="grid grid-cols-5 gap-6">
             {savedPosts?.map((post) => (
             <img
+            onClick={(e)=>handlePostOpen(post)}
               key={post?._id}
               src={post?.image}
               alt={post?.caption}
@@ -33,6 +44,9 @@ useEffect(()=>{
           </div>
         </div>
       </div>
+      {postOpen && (
+        <PostModal userId={userData?._id} post={selectedPost} closeModal={setPostOpen} commentCount={commentCount} setCommentCount={setCommentCount} />
+      )}
     </>
   );
 }
