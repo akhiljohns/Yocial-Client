f46593f4078b4b59c210210d117af930f88d8a4d
AKHIL JOHNS <akhiljohns250@gmail.com>
2024-05-08 11:33:01 +0530

    impleted video call room in caller side

--- src/pages/User/VideoCall/VideoCallInterface.jsx
+++ src/pages/User/VideoCall/VideoCallInterface.jsx
@@ -2,14 +2,17 @@ import React, { useEffect, useRef } from "react";
 import { useParams } from "react-router-dom";
 import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom"; // Import useNavigate
 
 const VideoCallInterface = () => {
   const { roomId } = useParams();
   const { userData } = useSelector((state) => state?.user);
   const elementRef = useRef(null);
+  const isJoinedRef = useRef(false); // Ref to track if room is already joined
+  const navigate = useNavigate(); // Initialize useNavigate
 
-  useEffect(() => {
-    const meeting = async () => {
+  const handleJoinRoom = () => {
+    if (!isJoinedRef.current) {
       const appId = parseInt(import.meta.env.VITE_APP_ID);
       const serverSecret = import.meta.env.VITE_SERVER_SECRET;
       const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
@@ -27,12 +30,26 @@ const VideoCallInterface = () => {
           mode: ZegoUIKitPrebuilt.OneONoneCall,
         },
       });
-    };
 
-    meeting();
-  }, [roomId, userData]);
+      isJoinedRef.current = true; // Update ref to indicate room is joined
+    }
+  };
 
-  return <div ref={elementRef}></div>;
+  useEffect(() => {
+    if (userData) {
+      handleJoinRoom();
+    }else{
+      navigate("/login");
+      return null;
+    }
+  }, [userData]);
+
+  return (
+    <div>
+      {/* Element to render video call interface */}
+      <div ref={elementRef}></div>
+    </div>
+  );
 };
 
 export default VideoCallInterface;
