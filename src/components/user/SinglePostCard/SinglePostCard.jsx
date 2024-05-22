3a7632f6f24c978d3b0cb1f25b8da858202c0fa5
AKHIL JOHNS <akhiljohns250@gmail.com>
2024-05-22 12:07:22 +0530

    likes,comments, likedusers split into components in singlepostcard

--- src/components/user/SinglePostCard/SinglePostCard.jsx
+++ src/components/user/SinglePostCard/SinglePostCard.jsx
@@ -19,6 +19,9 @@ import ReportIcon from "../Icons/ReportIcon";
 import "./style.css";
 import { emitPostInteraction } from "../../../services/User/SocketHandler";
 import { addNewReduxNotification } from "../../../utils/reducers/notificationReducer";
+import LikeButton from "./LikeButton";
+import CommentButton from "./CommentButton";
+import LikedStatus from "./LikedStatus";
 const SinglePostCard = ({ post, setLikePost, toggleLikesModal }) => {
   const dispatch = useDispatch();
   const user = useSelector((state) => state?.user?.userData);
@@ -54,7 +57,7 @@ const SinglePostCard = ({ post, setLikePost, toggleLikesModal }) => {
           user?.username
         ).then((resp) => {
           emitPostInteraction("postInteraction", {
-            postOwner:post?.userId._id,
+            postOwner: post?.userId._id,
             userId: user?._id,
             username: user.username,
             message: "liked Your post",
@@ -213,61 +216,15 @@ const SinglePostCard = ({ post, setLikePost, toggleLikesModal }) => {
 
       <div className="flex items-center justify-between text-gray-500">
         {/* Like Button */}
-        <button
-          onClick={likeunlike}
-          className="flex justify-center items-center gap-2 px-2 hover:bg-gray-50 rounded-full p-1"
-        >
-          <svg
-            color={liked ? "red" : ""}
-            className="w-5 h-5 fill-current"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-          >
-            <path d="M12 21.35l-1.45-1.32C6.11 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-4.11 6.86-8.55 11.54L12 21.35z" />
-          </svg>
-          <span>{post?.likes?.length} Likes</span>
-        </button>
+        <LikeButton likeunlike={likeunlike} liked={liked} likeLength={post?.likes?.length} />
         {/* Comment Button */}
-        <button
-          onClick={() => setCommentOpen(true)}
-          className="flex justify-center items-center gap-2 px-2 hover:bg-gray-50 rounded-full p-1"
-        >
-          <svg
-            width="22px"
-            height="22px"
-            viewBox="0 0 24 24"
-            className="w-5 h-5 fill-current"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fillRule="evenodd"
-              d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 13.5997 2.37562 15.1116 3.04346 16.4525C3.22094 16.8088 3.28001 17.2161 3.17712 17.6006L2.58151 19.8267C2.32295 20.793 3.20701 21.677 4.17335 21.4185L6.39939 20.8229C6.78393 20.72 7.19121 20.7791 7.54753 20.9565C8.88837 21.6244 10.4003 22 12 22ZM8 13.25C7.58579 13.25 7.25 13.5858 7.25 14C7.25 14.4142 7.58579 14.75 8 14.75H13.5C13.9142 14.75 14.25 14.4142 14.25 14C14.25 13.5858 13.9142 13.25 13.5 13.25H8ZM7.25 10.5C7.25 10.0858 7.58579 9.75 8 9.75H16C16.4142 9.75 16.75 10.0858 16.75 10.5C16.75 10.9142 16.4142 11.25 16 11.25H8C7.58579 11.25 7.25 10.9142 7.25 10.5Z"
-            />
-          </svg>
-          <span> {commentCount} Comments</span>
-        </button>
+        <CommentButton setCommentOpen={setCommentOpen} commentCount={commentCount}/>
         {/* Save Post Button */}
         <SavePostButton postId={post?._id} />
       </div>
       {/* POST LIKED USERS */}
-      <div
-        onClick={(e) => showLikes(post?.likes)}
-        className="mt-1  cursor-pointer"
-      >
-        {post?.likes?.length > 0 ? (
-          <span className="pl-2 text-black text-sm font-light select-none">
-            {post?.likes?.includes(user?._id)
-              ? `You ${
-                  post?.likes.length > 1
-                    ? `and ${post?.likes?.length - 1} other`
-                    : ""
-                } liked this post`
-              : post?.likes?.length === 1
-              ? `One person liked this post`
-              : `${post?.likes?.length} people liked this post`}
-          </span>
-        ) : null}
-      </div>
+        <LikedStatus showLikes={showLikes} postLikes={post?.likes} userId={user?._id} />
+
       {/* Comment Modal */}
       {commentOpen && (
         <PostModal
