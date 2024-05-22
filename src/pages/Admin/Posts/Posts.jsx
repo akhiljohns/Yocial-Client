eec0671b2ce65a03f925b8dbe5da40c42f6dbd66
AKHIL JOHNS <akhiljohns250@gmail.com>
2024-05-22 10:52:52 +0530

    added pagination in admin posts page

--- src/pages/Admin/Posts/Posts.jsx
+++ src/pages/Admin/Posts/Posts.jsx
@@ -12,11 +12,12 @@ const AdminPosts = () => {
   const [sortBy, setSortBy] = useState(""); // Sorting option
   const [filterBy, setFilterBy] = useState(""); // Filtering option
   const [searchQuery, setSearchQuery] = useState(""); // Search query
-
+  const [totalPages, setTotalPages] = useState(5);
   useEffect(() => {
-    fetchPosts(currentPage, perPage)
-      .then((response) => {
+    fetchPosts(currentPage, perPage, sortBy)
+      .then(async (response) => {
         setPosts(response.posts);
+        setTotalPages(Math.ceil(response.totalPosts / perPage));
       })
       .catch((error) => {
         errorToast(error);
@@ -37,6 +38,17 @@ const AdminPosts = () => {
 
   const getSortedPosts = () => {
     let sortedPosts = [...posts];
+
+    // Search functionality
+    if (searchQuery) {
+      const searchTerm = searchQuery.toLowerCase();
+      sortedPosts = sortedPosts.filter(
+        (post) =>
+          post.caption && post.caption.toLowerCase().includes(searchTerm)
+      );
+    }
+
+    // Sorting functionality
     if (sortBy === "date") {
       sortedPosts.sort((a, b) => (a.date > b.date ? 1 : -1));
     } else if (sortBy === "likes") {
@@ -75,79 +87,85 @@ const AdminPosts = () => {
             />
           </div>
           <div className="flex-1 h-[200px] flex flex-col gap-5 relative">
-            
             <div className="bg-white flex-1">
-            <table className="w-[100%] divide-y divide-gray-200">
-              <thead className="bg-gray-50">
-                <tr>
-                  <th
-                    scope="col"
-                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                  >
-                    Post Image
-                  </th>
-                  <th
-                    scope="col"
-                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                  >
-                    Post Owner
-                  </th>
-                  <th
-                    scope="col"
-                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                  >
-                    Post Caption
-                  </th>
-                  <th
-                    scope="col"
-                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                  >
-                    Like Count
-                  </th>
-                  <th
-                    scope="col"
-                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                  >
-                    Comment Count
-                  </th>
-                  <th
-                    scope="col"
-                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                  >
-                    Posted Date
-                  </th>
-                </tr>
-              </thead>
-              <tbody className="bg-white divide-y divide-gray-200">
-                {getSortedPosts().map((post, index) => (
-                  <PostsRow post={post} key={index} />
-                ))}
-              </tbody>
-            </table>
+              <table className="w-[100%] divide-y divide-gray-200">
+                <thead className="bg-gray-50">
+                  <tr>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Post Image
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Post Owner
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Post Caption
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Like Count
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Comment Count
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Posted Date
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Action
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="bg-white divide-y divide-gray-200">
+                  {getSortedPosts().map((post, index) => (
+                    <PostsRow post={post} key={index} />
+                  ))}
+                </tbody>
+              </table>
             </div>
-
-
 
             <div className="flex justify-center items-center w-full bottom-10 px-10">
               <div className="flex gap-5">
-              <button
-                onClick={handlePrevPage}
-                disabled={currentPage === 1}
-                className="bg-blue-500 text-white px-4 py-2 rounded-md"
-              >
-                Previous
-              </button>
-              <button
-                onClick={handleNextPage}
-                disabled={posts.length < 5 ? true : false}
-                className="bg-blue-500 text-white px-4 py-2 rounded-md"
-              >
-                Next
-              </button>
+                <button
+                  onClick={handlePrevPage}
+                  disabled={currentPage === 1}
+                  className="bg-blue-500 text-white px-4 py-2 rounded-md"
+                >
+                  Previous
+                </button>
+                {/* Pagination showing current page and total pages */}
+                <div className="text-white text-md font-sans font-bold px-4 py-2">
+                  Page {currentPage} of {totalPages}
+                </div>
+                <button
+                  onClick={handleNextPage}
+                  disabled={posts.length < 5 ? true : false}
+                  className="bg-blue-500 text-white px-4 py-2 rounded-md"
+                >
+                  Next
+                </button>
               </div>
+            </div>
           </div>
-          </div>
-          
         </div>
       </div>
     </>
