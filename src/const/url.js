bbbc12544787cafd2389b588d61d8dd4bd4c9a92
AKHIL JOHNS <akhiljohns250@gmail.com>
2024-06-06 10:25:19 +0530

    updated gitignore & production url

--- src/const/url.js
+++ src/const/url.js
@@ -1,5 +1,5 @@
-export const BASE_URL = "http://localhost:2500";
+// export const BASE_URL = "http://localhost:2500";
 export const URL = "http://localhost:3000";
 
 //for server
-// export const BASE_URL = "https://relink.thetrendly.shop";
+export const BASE_URL = "https://yocial.akhiljohns.site";
